a7c116ac4488ac6a6e8e3c1bce30a9f75ee1da0e
Rejeess Bhose <rejeessbhose@Rejeesss-MacBook-Pro.local>
2020-11-13 16:08:52 -0600

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '76ab68c280708342cce5a6664ced4d17'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '38465053-958a-43cc-bc26-02a4bd34c532'
+};
